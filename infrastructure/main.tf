be44deb856f7338e7f5dfc991442345d75e358a1
chedi bououn <chbouountito@gmail.com>
2026-07-30 16:09:34 +0100

    Feat: fixed parts including the new device feature and ci/cd

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "product-entreprise-student-rg"
-  location = "Central India"
+  name     = var.resource_group_name
+  location = var.location
 }
 
 
@@ -49,7 +49,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                = "product-entreprise-student-vm"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_B2ats_v2"
+  size                = var.vm_size
   admin_username      = "azureadminstudent"
   network_interface_ids = [
     azurerm_network_interface.nic.id,
@@ -108,7 +108,7 @@ resource "azurerm_network_security_group" "nsg"{
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
-    source_address_prefix      = file(var.my_ip_address)
+    source_address_prefix      = var.my_ip_address
     destination_address_prefix = "*"
   }
   security_rule{
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -10,3 +10,21 @@ variable "my_ip_address" {
   type        = string
   sensitive   = true
 }
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+  default     = "product-entreprise-student-rg"
+}
+
+variable "location" {
+  description = "The Azure region where resources will be created"
+  type        = string
+  default     = "Central India"
+}
+
+variable "vm_size" {
+  description = "The size of the virtual machine"
+  type        = string
+  default     = "Standard_B1s"
+}
+
